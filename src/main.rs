6bc34360c21881019502192827f42f7f6329694c
Juliette Cordor <juliette@jewelexx.com>
2023-04-05 14:21:40 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::{
-    io::{stdin, stdout, Write},
+    io::{stdin, Write},
     mem,
 };
 
 use clap::Parser;
+use rayon::prelude::*;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -16,20 +17,6 @@ struct Args {
 #[derive(Debug, Default)]
 struct Filler(u128, u128, u128, u128);
 
-fn fill_mem(max_el: usize, vec: &mut Vec<Filler>) {
-    let mut stdout = std::io::stdout();
-
-    for i in 0..10 {
-        write!(stdout, "\rFilling {}% RAM", (i + 1) * 10).unwrap();
-        stdout.flush().unwrap();
-
-        for _ in 0..(max_el / 10) {
-            // Initializes 32 bytes of memory and pushes it into memory
-            vec.push(Filler::default());
-        }
-    }
-}
-
 fn main() {
     let args = Args::parse();
     let max_el = {
@@ -40,23 +27,21 @@ fn main() {
     };
 
     // Create an empty vector, allowing us to push new `Filler`s into memory
-    let mut vec = Vec::<Filler>::new();
-    fill_mem(max_el, &mut vec);
+    let vec = vec![(); max_el];
+    let filled: Vec<Filler> = vec.into_par_iter().map(|_| Filler::default()).collect();
 
     println!("\nYour RAM has been filled :)");
 
     // Wait until the user is ready to clear memory
-    {
-        let mut stdout = std::io::stdout();
+    let mut stdout = std::io::stdout();
 
-        print!("Press Enter to clear memory");
-        stdout.flush().unwrap();
-        let mut str = String::new();
-        stdin().read_line(&mut str).unwrap();
-    }
+    print!("Press Enter to clear memory");
+    stdout.flush().unwrap();
+    let mut str = String::new();
+    stdin().read_line(&mut str).unwrap();
 
     // Clear memory "instantly"
-    drop(vec);
+    drop(filled);
 
     println!("\nMemory Cleared...");
 }
